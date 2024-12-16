006ded8c93f0d9e0ecc6dd69b8aab2f9a67ddfdf
Krzysztof Szymaniak <k-szymaniak@tlen.pl>
2024-12-16 21:25:38 +0100

    16.12.2024

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -31,6 +31,7 @@ function Home({ user }) {
         </div>
       </header>
 
+      {/* Sekcja 1 */}
       <section style={styles.section}>
         <h2 style={styles.heading}>Osoby potrzebujące twojej pomocy!</h2>
         <div style={styles.posts}>
@@ -47,9 +48,9 @@ function Home({ user }) {
                   )}
                 </div>
                 <div style={styles.textContent}>
-                  <h3 style={styles.postTitle}>{post.title}</h3> {/* Centered */}
-                  <p style={styles.postDescription}>{post.description}</p> {/* Justified */}
-                  <p style={styles.postTags}><strong>Tags:</strong> {post.tags}</p> {/* Justified */}
+                  <h3 style={styles.postTitle}>{post.title}</h3>
+                  <p style={styles.postDescription}>{post.description}</p>
+                  <p style={styles.postTags}><strong>Tags:</strong> {post.tags}</p>
                   <p
                     style={{
                       ...styles.postHelpType,
@@ -72,10 +73,39 @@ function Home({ user }) {
         </div>
       </section>
 
+      {/* Sekcja 2 */}
+      <section style={styles.infoSection}>
+        <div style={styles.infoContent}>
+          <h2 style={styles.heading}>Pomoc w prosty sposób</h2>
+          <p style={styles.akapit}>
+          ConnectCare to wyjątkowe miejsce, stworzone z myślą o tych, którzy chcą pomagać innym, a jednocześnie nie boią się poprosić o wsparcie, gdy tego potrzebują. To przestrzeń, gdzie możesz dzielić się swoimi doświadczeniami, udzielać rad, a także korzystać z mądrości i pomocy innych użytkowników.
+
+Rejestracja na platformie jest szybka i intuicyjna – wystarczy kilka kroków, aby dołączyć do naszej społeczności. Po założeniu konta masz możliwość stworzenia swojego pierwszego posta. Możesz opisać sytuację, z którą się mierzysz, lub zaoferować pomoc w obszarze, w którym masz doświadczenie czy kompetencje. To doskonała okazja, by wpłynąć na życie innych i jednocześnie dostrzec, jak wiele dobrego możesz otrzymać w zamian.
+          </p>
+        </div>
+        <div style={styles.imageContainer}>
+          <img src="/img/support-2.jpg" alt="Help Us" style={styles.infoImage} />
+        </div>
+      </section>
+
+      {/* Sekcja 3 */}
+      <section style={styles.infoSectionReversed}>
+        <div style={styles.infoContent}>
+          <h2 style={styles.heading}>Łączymy wolontariuszy i osoby w potrzebie</h2>
+          <p style={styles.akapit}>
+            Nasza platforma umożliwia szybkie połączenie ludzi, którzy oferują pomoc, z tymi, którzy jej potrzebują.
+            To miejsce, w którym nie ma barier – każdy może się włączyć i pomóc.
+          </p>
+        </div>
+        <div style={styles.imageContainer}>
+          <img src="/img/support.jpg" alt="Volunteers" style={styles.infoImage} />
+        </div>
+      </section>
+
       {user && user.role === 'Osoba potrzebująca' && (
         <section style={styles.createPostSection}>
           <Link to="/create_post">
-            <button style={styles.createPostButton}>Create Post</button>
+            <button style={styles.createPostButton}>Utwórz Post</button>
           </Link>
         </section>
       )}
@@ -89,7 +119,7 @@ function Home({ user }) {
 
 const styles = {
   container: {
-    fontFamily: 'Poppins, Arial, sans-serif',
+    fontFamily: 'poppins, sans-serif',
     textAlign: 'center',
     margin: '0 auto',
     padding: '0px',
@@ -97,7 +127,7 @@ const styles = {
     backgroundColor: '#f9f9f9',
   },
   header: {
-    backgroundImage: 'url("/img/header.jpg")', // Corrected image path
+    backgroundImage: 'url("/img/header.jpg")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
@@ -108,6 +138,9 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative',
+  },
+  akapit: {
+    textAlign: 'justify'
   },
   headerOverlay: {
     position: 'absolute',
@@ -127,7 +160,7 @@ const styles = {
     textAlign: 'center',
   },
   heading: {
-    fontSize: '2.5rem', // Increased size
+    fontSize: '2.5rem',
     fontWeight: 'bold',
     marginBottom: '30px',
     color: '#333',
@@ -148,42 +181,74 @@ const styles = {
   },
   imageContainer: {
     marginBottom: '15px',
+    width: '100%', // Increased the width of images
+    float: 'left',
+    marginRight: '20px',
+  },
+  infoSection: {
+    backgroundColor: '#f1f1f1',
+    padding: '40px 50px',
+    marginTop: '50px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoSectionReversed: {
+    backgroundColor: '#fff',
+    padding: '40px 50px',
+    marginTop: '50px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row-reverse',
+  },
+  infoContent: {
+    textAlign: 'center',
+    maxWidth: '50%',
+    margin: '0 auto',
+    padding: '50px',
+  },
+  infoImage: {
+    width: '100%', // Increased the width of images to 70%
+    maxWidth: '100%',
+    marginTop: '20px',
+    borderRadius: '8px',
   },
   image: {
     width: '100%',
     height: 'auto',
-    maxHeight: '200px',
+    maxHeight: 'auto', // Increased max height for images
     borderRadius: '8px',
     objectFit: 'cover',
   },
   textContent: {
-    textAlign: 'left', // Keep other content left-aligned
+    textAlign: 'left',
   },
   postTitle: {
     color: '#333',
     fontSize: '1.5rem',
     fontWeight: 'bold',
     marginBottom: '10px',
-    textAlign: 'center', // Center title
+    textAlign: 'center',
   },
   postDescription: {
     fontSize: '1rem',
     color: '#555',
     marginBottom: '10px',
-    textAlign: 'justify', // Justified description
+    textAlign: 'justify',
   },
   postTags: {
     fontSize: '1rem',
     color: '#555',
     marginBottom: '10px',
-    textAlign: 'justify', // Justified tags
+    textAlign: 'justify',
   },
   postHelpType: {
     fontSize: '1rem',
     color: '#333',
     textAlign: 'center',
     padding: '5px 10px',
-    borderRadius: '5px', // Rounded edges
+    borderRadius: '5px',
     display: 'inline-block',
     marginBottom: '10px',
   },
@@ -194,10 +259,8 @@ const styles = {
     borderRadius: '5px',
     padding: '10px 20px',
     cursor: 'pointer',
-    fontSize: '20px',
+    fontSize: '14px',
     marginTop: '10px',
-    width: '100%', // Full width button
-    fontWeight: '600',
   },
   createPostButton: {
     backgroundColor: '#28a745',
@@ -223,3 +286,4 @@ const styles = {
 };
 
 export default Home;
+
